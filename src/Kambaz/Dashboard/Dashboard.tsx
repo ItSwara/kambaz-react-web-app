e58426fd588500e3cf861715abb5bcf2ef369969
Swarali Degaonkar <swarali2809@gmail.com>
2025-03-12 20:31:32 -0400

    removed unsued variables

--- src/Kambaz/Dashboard/Dashboard.tsx
+++ src/Kambaz/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import  { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {  Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
